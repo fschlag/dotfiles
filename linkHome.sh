4006a0f14105a67b65071fcfeacc0938b63e863c
Florian Schlag <git@florianschlag.de>
2022-08-11 08:54:39 +0200

    fix: wrong quotation

--- linkHome.sh
+++ linkHome.sh
@@ -16,18 +16,18 @@ log "Running $0 on $(date)"
 find $WORK_DIR/home -type f | while IFS= read -r FILE
 do
     FILE_FULL_PATH="$($READLINK -f "$FILE")"
-    FILE_RELATIVE_PATH="$(echo $FILE | sed "s!^${WORK_DIR}/home/!!")"
+    FILE_RELATIVE_PATH="$(echo "$FILE" | sed "s!^${WORK_DIR}/home/!!")"
     
     log "Installing $FILE_RELATIVE_PATH"
     PATH_TO_LINK="${HOME}/${FILE_RELATIVE_PATH}"
-    PATH_TO_LINK_DIR="$(dirname ${PATH_TO_LINK})"
+    PATH_TO_LINK_DIR="$(dirname "${PATH_TO_LINK}")"
 
     if [ ! -d "$PATH_TO_LINK_DIR" ]; then
         log "Creating directory $PATH_TO_LINK_DIR"
         mkdir -p "$PATH_TO_LINK_DIR"
     fi
     
-    if [ "$FILE_FULL_PATH" == "$($READLINK -f $PATH_TO_LINK)" ]; then
+    if [ "$FILE_FULL_PATH" == "$($READLINK -f "$PATH_TO_LINK")" ]; then
         log "$PATH_TO_LINK already installed"
     else
         if [ -f "$PATH_TO_LINK" ]; then
