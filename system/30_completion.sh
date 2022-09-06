fcec70eaa0945daa78c8748c7108e03a5a02a5cd
Florian Schlag <git@florianschlag.de>
2022-09-06 11:24:09 +0200

    Add completion for OpenShift CLI

--- system/30_completion.sh
+++ system/30_completion.sh
@@ -16,6 +16,11 @@ if command -v kubectl > /dev/null; then
     source <(kubectl completion zsh)
 fi
 
+# OpenShift CLI
+if command -v oc > /dev/null; then
+    source <(oc completion zsh)
+fi
+
 # k9s
 if command -v k9s > /dev/null; then
     source <(k9s completion zsh)
