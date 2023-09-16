ffeebc3643c0e2fb1e3cb70ae3c2708fc973ae85
Florian Schlag <git@florianschlag.de>
2023-09-16 15:48:53 +0200

    add ruby path

--- system/10_path.sh
+++ system/10_path.sh
@@ -23,3 +23,8 @@ export SDKMAN_DIR="$HOME/.sdkman"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+  export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
