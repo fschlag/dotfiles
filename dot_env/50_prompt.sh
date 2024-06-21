c4ac74a1fcd58f974647fe03b76aa8bb02b8929a
Florian Schlag <git@florianschlag.de>
2024-06-21 18:12:00 +0200

    only load p10k on zsh

--- dot_env/50_prompt.sh
+++ dot_env/50_prompt.sh
@@ -1,2 +1,4 @@
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+if ps -p$$ -ocmd= | grep "zsh"; then
+    [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+fi
