a4c13181965860320443100edd073f7eb960996b
Florian Schlag <git@florianschlag.de>
2024-06-25 08:39:18 +0200

    fix shell detection

--- dot_env/50_prompt.sh
+++ dot_env/50_prompt.sh
@@ -1,4 +1,4 @@
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-if ps -p$$ -ocmd= | grep "zsh"; then
+if echo "$SHELL" | grep -q "zsh"; then
     [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 fi
