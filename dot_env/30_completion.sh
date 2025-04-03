b01e4b1faee235ae54026d53a0dc1087643f4788
Schlag, Florian <git@florianschlag.de>
2025-04-03 14:39:04 +0200

    add completion for yq

--- dot_env/30_completion.sh
+++ dot_env/30_completion.sh
@@ -14,7 +14,7 @@ if command -v ng > /dev/null; then
     source <(ng completion script)
 fi
 
-COMPLETION_SCRIPTS=( kubectl oc k9s helm datree argocd podman flux)
+COMPLETION_SCRIPTS=( kubectl oc k9s helm datree argocd podman flux yq)
 
 for SCRIPT in "${COMPLETION_SCRIPTS[@]}"
 do
